fc454d88cd1d18620a4bf6cc0181993bbe6e69ca
zhenghui <wzhdx123@126.com>
2022-07-16 20:47:31 +0800

    220716 finetune and position embedding fixed

--- models/mmht_model.py
+++ models/mmht_model.py
@@ -73,13 +73,10 @@ class mmhtModel(BaseModel):
         b = self.opt.batch_size
         self.pixel_pos = pos.unsqueeze(0).repeat(b, 1, 1, 1).to(self.device)
         self.pixel_pos = self.pixel_pos.flatten(2).permute(2, 0, 1)
-        print('pixel shape:', self.pixel_pos.shape)
 
         clip_pos = util.ClipPositionEmbeddingSine().unsqueeze(0).repeat(b, 1, 1).permute(1, 0, 2).to(self.device)
-        print('clip_pos shape:', clip_pos.shape)
         # clip_pos = torch.zeros((50, b, 256)).to(self.device)
         self.pixel_pos = torch.cat([clip_pos, self.pixel_pos], 0)
-        print('after concat pixel_pos shape:', self.pixel_pos.shape)
 
         self.patch_pos = patch_pos.unsqueeze(0).repeat(b, 1, 1, 1).to(self.device)
         self.patch_pos = self.patch_pos.flatten(2).permute(2, 0, 1)
